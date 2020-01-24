639574ccc14f5f6ecef28f7c73eb4cfa1cfcf050
Bijan <bafghani@ucsd.edu>
2020-01-24 21:03:17 +0000

    fixed delete

--- src/bst/BST.hpp
+++ src/bst/BST.hpp
@@ -120,7 +120,7 @@ class BST {
             if (curr == nullptr) {
                 return BST<Data>::iterator(nullptr);
             }
-                }
+        }
         return BST<Data>::iterator(curr);
     }
 
@@ -134,7 +134,9 @@ class BST {
             return false;
         }
         BSTNode<Data>* curr = root;
+
         // while curr->getData() != item
+
         while (!(!(curr->getData() < item) && !(item < curr->getData()))) {
             if (item < curr->getData()) {
                 curr = curr->left;
@@ -157,203 +159,247 @@ class BST {
                     curr->parent->right = nullptr;
                 }
             }
+            isize--;
             delete curr;
         }
 
         // case 2 (1 child)
         else if (curr->left == nullptr || curr->right == nullptr) {
             // curr->parent point to curr->child instead of to curr
-            if (curr->left == nullptr) {
-                if (curr->parent != nullptr) {
-                    if (curr = curr->parent->right) {
-                        curr->parent->right = curr->right;
-                    } else {
-                        curr->parent->left = curr->right;
-                    }
+            if (curr = root) {
+                // root has right child
+                if (curr->left == nullptr) {
+                    root = curr->right;
+                    curr->right->parent = nullptr;
                 }
-
-                delete curr;
-            } else {
-                if (curr->parent != nullptr) {
-                    if (curr = curr->parent->right) {
-                        curr->parent->right = curr->left;
-                    } else {
-                        curr->parent->left = curr->left;
-                    }
+                // root has left child
+                else {
+                    root = curr->left;
+                    curr->left->parent = nullptr;
                 }
-                delete curr;
             }
+            // curr has a right child
+            else if (curr->left == nullptr) {
+                // curr is its parents left child
+                if (curr = curr->parent->left) {
+                    curr->parent->left = curr->right;
+                    curr->right->parent = curr->parent;
+                }
+                // curr is its parents right child
+                else {
+                    curr->parent->right = curr->right;
+                    curr->right->parent = curr->parent;
+                }
+            }
+            // curr has a left child
+            else if (curr->right == nullptr) {
+                // curr is its parents left child
+                if (curr = curr->parent->left) {
+                    curr->parent->left = curr->left;
+                    curr->left->parent = curr->parent;
+                }
+                // curr is its parents right child
+                else {
+                    curr->parent->right = curr->left;
+                    curr->left->parent = curr->parent;
+                }
+            }
+            isize--;
+            delete curr;
+
         }
 
         // case 3 (2 children)
         else {
             BSTNode<Data>* s = curr->successor();
-            if (s->parent != nullptr) {
-                if (s == s->parent->left) {
-                    s->parent->left = s->right;
-                } else {
-                    s->parent->right = s->right;
+
+            if (s = s->parent->right) {
+                // case 1 s is right child with right child
+                if (s->right != nullptr) {
+                    s->right = s->right->right;
+                    s->right->parent = s;
+                }
+                // case 2 s is right child with no children
+                else if (s->right == nullptr && s->left == nullptr) {
+                    s->right = nullptr;
                 }
             }
-            curr->setData(s->getData());
+
+            // left child
+            else {
+                // case 3 s is left child with no children
+                if (s->right == nullptr && s->right == nullptr) {
+                    s->parent->left = nullptr;
+                }
+                if (s->right != nullptr) {
+                    s->right->parent = s->parent;
+                    s->parent->left = s->right;
+                }
+                curr->setData(s->getData());
+            }
             delete s;
+            isize--;
         }
-        isize--;
+    
+    iheight = height();
+    return true;
+}
+
+/** returns the number of nodes in the BST*/
+unsigned int
+size() const {
+    return isize;
+}
+
+/** returns the maximum depth of the BST */
+int height() const { return getHeight(root); }
+
+/** returns true if there are no nodes in the BST, false otherwise */
+bool empty() const {
+    if (isize == 0) {
         return true;
+    } else {
+        return false;
     }
+}
 
-    /** returns the number of nodes in the BST*/
-    unsigned int size() const { return isize; }
+/** returns an iterator pointing to the root of the BST */
+iterator begin() const { return BST<Data>::iterator(first(root)); }
 
-    /** returns the maximum depth of the BST */
-    int height() const { return getHeight(root); }
+/** Return an iterator pointing past the last item in the BST. */
+iterator end() const { return typename BST<Data>::iterator(0); }
 
-    /** returns true if there are no nodes in the BST, false otherwise */
-    bool empty() const {
-        if (isize == 0) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    /** returns an iterator pointing to the root of the BST */
-    iterator begin() const { return BST<Data>::iterator(first(root)); }
-
-    /** Return an iterator pointing past the last item in the BST. */
-    iterator end() const { return typename BST<Data>::iterator(0); }
-
-    /** Returns a vector containing the elements of our BST
-     * in the order of the inorder traversal. (Left, Root, Right)
-     */
-    vector<Data> inorder() const {
-        vector<Data> list;
-        if (root == nullptr) {
-            return list;
-        }
-        inOrderHelper(root, list);
+/** Returns a vector containing the elements of our BST
+ * in the order of the inorder traversal. (Left, Root, Right)
+ */
+vector<Data> inorder() const {
+    vector<Data> list;
+    if (root == nullptr) {
         return list;
     }
+    inOrderHelper(root, list);
+    return list;
+}
 
-    /**
-     * DO NOT CHANGE THIS METHOD
-     * Prints a tree, including its structure to an
-     * output stream to help with grading and debugging.
-     */
-    void print(ostream* out) {
-        if (empty()) {
-            *out << "(Empty tree)" << endl;
-            return;
-        }
+/**
+ * DO NOT CHANGE THIS METHOD
+ * Prints a tree, including its structure to an
+ * output stream to help with grading and debugging.
+ */
+void print(ostream* out) {
+    if (empty()) {
+        *out << "(Empty tree)" << endl;
+        return;
+    }
 
-        // Using a queue to traverse a tree is otherwise
-        // known as a breadth first traversal of the tree.
-        // It's useful here to show which nodes are on which
-        // level.
-        queue<BSTNode<Data>*> toVisit;
-        toVisit.push(root);
-        int nodesPerLevel = 1;
-        int totalSpacing = 1 << iheight;
+    // Using a queue to traverse a tree is otherwise
+    // known as a breadth first traversal of the tree.
+    // It's useful here to show which nodes are on which
+    // level.
+    queue<BSTNode<Data>*> toVisit;
+    toVisit.push(root);
+    int nodesPerLevel = 1;
+    int totalSpacing = 1 << iheight;
 
-        for (int i = 0; i <= iheight; i++) {
-            for (int j = 0; j < nodesPerLevel; j++) {
-                BSTNode<Data>* curr = toVisit.front();
-                toVisit.pop();
-                if (curr == nullptr) {
-                    *out << "X";
-                    // If we have an entire missing subtree, we
-                    // would like to print X's in place of all
-                    // nodes because it avoids ambiguity
-                    toVisit.push(nullptr);
-                    toVisit.push(nullptr);
-                } else {
-                    *out << curr->getData();
-                    toVisit.push(curr->left);
-                    toVisit.push(curr->right);
-                }
-                for (int k = 0; k < totalSpacing / nodesPerLevel; k++) {
-                    *out << "\t";
-                }
+    for (int i = 0; i <= iheight; i++) {
+        for (int j = 0; j < nodesPerLevel; j++) {
+            BSTNode<Data>* curr = toVisit.front();
+            toVisit.pop();
+            if (curr == nullptr) {
+                *out << "X";
+                // If we have an entire missing subtree, we
+                // would like to print X's in place of all
+                // nodes because it avoids ambiguity
+                toVisit.push(nullptr);
+                toVisit.push(nullptr);
+            } else {
+                *out << curr->getData();
+                toVisit.push(curr->left);
+                toVisit.push(curr->right);
             }
-            *out << endl;
-            nodesPerLevel *= 2;
+            for (int k = 0; k < totalSpacing / nodesPerLevel; k++) {
+                *out << "\t";
+            }
         }
+        *out << endl;
+        nodesPerLevel *= 2;
+    }
+}
+
+private:
+/** TODO Helper function for begin() */
+static BSTNode<Data>* first(BSTNode<Data>* root) {
+    BSTNode<Data>* curr = root;
+    if (curr == nullptr) {
+        return nullptr;
     }
 
-  private:
-    /** TODO Helper function for begin() */
-    static BSTNode<Data>* first(BSTNode<Data>* root) {
-        BSTNode<Data>* curr = root;
-        if (curr == nullptr) {
-            return nullptr;
-        }
-
-        while (curr->left != nullptr) {
-            curr = curr->left;
-        }
-
-        return curr;
+    while (curr->left != nullptr) {
+        curr = curr->left;
     }
 
-    /** Recursively deletes all Nodes of the BST
-     *  Helper method for BST destructor
-     */
-    static void deleteAll(BSTNode<Data>* n) {
-        // if current node is null: return;
-        if (n == nullptr) {
-            return;
-        }
-        deleteAll(n->left);   // recursively delete left sub-tree
-        deleteAll(n->right);  // recursively delete right sub-tree
+    return curr;
+}
 
-        // delete current node
-        delete n;
+/** Recursively deletes all Nodes of the BST
+ *  Helper method for BST destructor
+ */
+static void deleteAll(BSTNode<Data>* n) {
+    // if current node is null: return;
+    if (n == nullptr) {
+        return;
+    }
+    deleteAll(n->left);   // recursively delete left sub-tree
+    deleteAll(n->right);  // recursively delete right sub-tree
+
+    // delete current node
+    delete n;
+}
+
+/** TODO */
+BSTNode<Data>* buildSubtree(vector<Data>& data, int start, int end, int depth) {
+    if (start > end) {
+        return nullptr;
     }
 
-    /** TODO */
-    BSTNode<Data>* buildSubtree(vector<Data>& data, int start, int end,
-                                int depth) {
-        if (start > end) {
-            return nullptr;
-        }
+    int mid = (start + end) / 2;
+    BSTNode<Data>* root = new BSTNode<Data>(data[mid]);
+    isize++;
 
-        int mid = (start + end) / 2;
-        BSTNode<Data>* root = new BSTNode<Data>(data[mid]);
-        isize++;
+    root->left = buildSubtree(data, start, mid - 1, depth);
+    root->left->parent = root;
+    depth++;
+    root->right = buildSubtree(data, mid + 1, end, depth);
+    root->right->parent = root;
 
-        root->left = buildSubtree(data, start, mid - 1, depth);
-        root->left->parent = root;
-        depth++;
-        root->right = buildSubtree(data, mid + 1, end, depth);
-        root->right->parent = root;
+    return root;
+}
 
-        return root;
+// Add more helper functions below
+// Helper Method to determine maximum Depth of BST
+int getHeight(BSTNode<Data>* n) const {
+    if (n == nullptr) {
+        return -1;
     }
+    int leftSize = getHeight(n->left);
+    int rightSize = getHeight(n->right);
 
-    // Add more helper functions below
-    // Helper Method to determine maximum Depth of BST
-    int getHeight(BSTNode<Data>* n) const {
-        if (n == nullptr) {
-            return -1;
-        }
-        int leftSize = getHeight(n->left);
-        int rightSize = getHeight(n->right);
-
-        if (rightSize < leftSize) {
-            return leftSize + 1;
-        } else {
-            return rightSize + 1;
-        }
+    if (rightSize < leftSize) {
+        return leftSize + 1;
+    } else {
+        return rightSize + 1;
     }
-    // inOrderHelper
-    static void inOrderHelper(BSTNode<Data>* n, vector<Data>& addList) {
-        if (n == nullptr) {
-            return;
-        }
-        inOrderHelper(n->left, addList);
-        addList.push_back(n->getData());
-        inOrderHelper(n->right, addList);
+}
+// inOrderHelper
+static void inOrderHelper(BSTNode<Data>* n, vector<Data>& addList) {
+    if (n == nullptr) {
+        return;
     }
-};
+    inOrderHelper(n->left, addList);
+    addList.push_back(n->getData());
+    inOrderHelper(n->right, addList);
+}
+}
+;
 
 #endif  // BST_HPP
